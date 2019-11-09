b7d00e205cbe4fa5d5d0676977083d6b906e535d
Andrey Goryachev <andrey@sixdays.ru>
2019-11-09 17:52:40 +0300

    Goals parameter in settings

--- src/Generator/RandomMatchScoreGenerator.php
+++ src/Generator/RandomMatchScoreGenerator.php
@@ -4,9 +4,17 @@ namespace App\Generator;
 
 class RandomMatchScoreGenerator implements MatchScoreGeneratorInterface
 {
+    /** @var int */
+    private $maxGoalsPerMatch;
+
+    public function __construct(int $maxGoalsPerMatch)
+    {
+        $this->maxGoalsPerMatch = $maxGoalsPerMatch;
+    }
+
     public function generate(): array
     {
-        $pointsSum   = random_int(0, 6);
+        $pointsSum   = random_int(0, $this->maxGoalsPerMatch);
         $firstScore   = random_int(0, $pointsSum);
         $secondScore = $pointsSum - $firstScore;
         return [$firstScore, $secondScore];
